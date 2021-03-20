5a43f44c5b9186ab26173efd636c63d73cb3f81f
Noah Ogrydziak <12644755+notarobot767@users.noreply.github.com>
2021-03-20 00:39:09 -0400

    added notes

--- config.sh
+++ config.sh
@@ -2,31 +2,31 @@
 
 NETWORK="SERVERS"
 #default docker network to use
-
-LOGDIR="/disk2/logs/chrony"
-#local dir for logs
-
-DNS="10.0.20.10"
-#local dns server container for network
-
-DNS_SEARCH="ogrydziak.net"
-#local dns domain search
+#bridged podman network
 
 RESTART_MODE="on-failure"
 #docker restart mode
+#(syntax) --restart string
+#("always"|"no"|"on-failure"|"unless-stopped")
 
 TZ="America/New_York"
-#docker time zone
+#time zone
+#https://docs.diladele.com/docker/timezones.html
 
 PUID="1024"
-#docker UID
-
 PGID=$PUID
-#docker GID
-
 UMASK_SET="022"
+#enviornment vars for linuxserver images
+#https://hub.docker.com/u/linuxserver
 
 BINDING_IP="10.0.20.10"
 BINDing_IP2="10.0.20.9"
+#default IP interface binding
+#allows to listen on a speicific IP instead on all interfaces
+#useful for bind9 when dnsmasq uses all interfaces
+#bind9 would otherwise throw error bc the port was in use
 
 PULL_MODE="always"
+#pull image before creating
+#(syntax) --pull string
+#("always"|"missing"|"never") (default "missing")
